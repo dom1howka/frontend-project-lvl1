95977f38e2c28796be48af0d71d7cf358ad56630
dom1howka <dom1howka@gmail.com>
2022-12-21 12:34:35 +0300

    even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,26 +1,4 @@
 #!/usr/bin/env node
-import basisOfGames from '../index.js';
-import getRandomInt from '../getRandomInt.js';
+import evenGame from '../src/games/even.js';
 
-const evenGame = () => {
-    // Задание игры
-  const noteToEven = 'Answer "yes" if the number is even, otherwise answer "no".';
-  
-
-    // Вводные данные на игру
-    const taskEven = () => {
-    const number = getRandomInt(100);
-
-    // Определяем функцию для проверки на четность числа
-    const isEven = (num) => num % 2 === 0;
-
-    const question = `${number}`;
-    const result = (isEven(number) ? 'yes' : 'no');
-
-    return [question, result];
-  };
-
-  basisOfGames(noteToEven, taskEven);
-};
-
-export default evenGame;
+evenGame();
