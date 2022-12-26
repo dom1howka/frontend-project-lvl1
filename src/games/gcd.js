bdc472dc6f9f3b752280581939daed9fbe17807c
dom1howka <dom1howka@gmail.com>
2022-12-26 19:36:59 +0300

    gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import basisOfGames from '../index.js';
+import getRandomInt from '../getRandomInt';
+
+const gcd = () => {
+    const noteToGcd = 'Find the greatest common divisor of given numbers.';
+
+    const taskGcd = () => {
+        const number1 = getRandomInt(100);
+        const number2 = getRandomInt(100);
+
+        const question = `${number1} ${number2}`;
+
+        const getGcd = (num1, num2) => {
+            if (num1 % num2 === 0) {return num2; }
+            return getGcd(num2, num1 % num2); 
+        };
+        const result = getGcd(number1, number2).toString();
+        return [question, result];
+    };
+    basisOfGames(noteToGcd, taskGcd);
+};
+export default gcd;
