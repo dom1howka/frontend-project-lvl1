79ab43476336f176bb7ffb2356fd517c93eb3059
dom1howka <dom1howka@gmail.com>
2023-03-04 17:05:13 +0300

    fix

--- src/games/even.js
+++ src/games/even.js
@@ -2,12 +2,12 @@ import runEngine from "../index.js";
 import getRandomInRange from "../getRandomInRange.js";
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const number = getRandomInRange(1, 20);
+const isEven = (num) => num % 2 === 0;
 const makeRound = () => {
-  const isEven = (num) => num % 2 === 0;
   const question = `${number}`;
   const answer = isEven(number) ? "yes" : "no";
-  return [question, answer];
-  const number = getRandomInRange(1, 20);
+  return [question, answer]; 
 };
 
 export default () => {
