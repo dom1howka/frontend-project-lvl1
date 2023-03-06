c00973e38d67d93100d2d5a779800e4c63c82eef
dom1howka <dom1howka@gmail.com>
2023-03-06 17:12:03 +0300

    fix

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,17 +6,17 @@ const findGcd = (num1, num2) => {
   if (num1 % num2 === 0) {
     return num2;
   }
-  return getGcd(num2, num1 % num2);
+  return findGcd(num2, num1 % num2);
 };
 
-const getGcd = () => {
+const makeRound = () => {
   const number1 = getRandomInRange(1, 22);
   const number2 = getRandomInRange(1, 22);
   const question = `${number1} ${number2}`;
-  const result = String(getGcd(number1, number2));
+  const result = String(makeRound(number1, number2));
   return [question, result];
 };
 
 export default () => {
-  runEngine(gameRules, getGcd);
+  runEngine(gameRules, makeRound);
 };
